378e6c0b9357659923602688e7ccb828b41f435d
Lazynerd-007 <b.odunewu@lazynerdstudios.com>
2026-03-17 10:06:35 +0000

    upd

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,11 @@
 import Login from '@/component/login/Login'
 import React from 'react'
 
+export const metadata = {
+  title: 'Login - PursFinance',
+  description: 'Login to your PursFinance dashboard',
+}
+
 const page = () => {
   return (
       <Login />
